1aebb879808d23a3b4b7b373f28d13da56c23c5a
Djimon Jayasundera <woofless77@gmail.com>
2018-03-14 08:44:22 +0800

    C-Style and organisation

---
+++ Module 1/leapYear.c
@@ -0,0 +1,29 @@
+
+// By Alistair Parkinson, z5218710 and Ashley Porter z5220543
+// WA-MWH
+// 9/3/2018
+// This program determines whether a year is a leap year
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define START_OF_GREG_CALENDAR 1582
+
+int main () {
+    int year;
+    printf ("please enter the year you are interested in\n");
+    scanf ("%d", &year);
+    assert (year > START_OF_GREG_CALENDAR);
+	
+    if (year % 400 == 0) {
+        printf ("%d is a leap year!\n", year);
+    } else if (year % 100 == 0) {
+        printf ("%d is not a leap year!\n", year);
+    } else if (year % 4 == 0) {
+        printf ("%d is a leap year!\n", year);
+    } else {
+        printf ("%d is not a leap year!\n", year);
+    }
+    return EXIT_SUCCESS;
+}
